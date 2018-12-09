a632cae52f43e5c5a3ad9fce85b1fab6b9ac7b4f
dogmax <walus91@gmail.com>
2018-12-09 16:02:49 +0000

    Create ToxicTagger.lua

---
+++ ToxicTagger.lua
@@ -0,0 +1,25 @@
+function toxictagger(S, x)
+ScriptErrors:SetScript("OnShow", function(m) UIErrorsFrame:AddMessage("No target found",1,0,0) ScriptErrors:Hide() end)
+		
+		
+			function TryTake()
+			TargetNearestEnemy();
+				for i=1,table.getn(x) do
+			
+					if (UnitHealth("target")==0 or UnitIsTapped("target") or not (string.find(UnitName("target"), x[i]))) then
+							UIErrorsFrame:AddMessage("Looking for: "..x[i].." but didn't tag. Found: "..UnitName("target"),1,0,0); 
+							
+						
+					else 
+						if (string.find(UnitName("target"), x[i])) then 
+							CastSpellByName(S);
+							UIErrorsFrame:AddMessage("Attempting "..S.." on: "..UnitName("target").." reason: "..x[i],1,0,0);
+							break;
+						end
+					end
+				end				
+			end
+		
+        TryTake();
+ScriptErrors:SetScript("OnShow", function(m) ScriptErrors:Show() end)
+end
